c0162b027a55e278af070c27da504c7b2f634c47
JGirCo <juanmanuelgiraldoc@gmail.com>
2023-12-23 23:23:13 -0500

    Initial commit

--- init.lua
+++ init.lua
@@ -18,7 +18,7 @@ return {
   },
 
   -- Set colorscheme to use
-  colorscheme = "astrodark",
+  colorscheme = "rose-pine",
 
   -- Diagnostics configuration (for vim.diagnostics.config({...})) when diagnostics are on
   diagnostics = {
@@ -52,6 +52,18 @@ return {
     servers = {
       -- "pyright"
     },
+    -- config = {
+    --   rust_analyzer = {
+    --     settings = {
+    --       ["rust-analyzer"] = {
+    --         cargo = {
+    --           extraEnv = { CARGO_PROFILE_RUST_ANALYZER_INHERITS = 'dev', },
+    --           extraArgs = { "--profile", "rust-analyzer", },
+    --         },
+    --       },
+    --     },
+    --   },
+    -- },
   },
 
   -- Configure require("lazy").setup() options
@@ -82,4 +94,21 @@ return {
     --   },
     -- }
   end,
+
+  {
+    "nvim-telescope/telescope.nvim",
+    dependencies = { -- add a new dependency to telescope that is our new plugin
+      "nvim-telescope/telescope-media-files.nvim",
+    },
+    -- the first parameter is the plugin specification
+    -- the second is the table of options as set up in Lazy with the `opts` key
+    config = function(plugin, opts)
+      -- run the core AstroNvim configuration function with the options table
+      require("plugins.configs.telescope")(plugin, opts)
+
+      -- require telescope and load extensions as necessary
+      local telescope = require "telescope"
+      telescope.load_extension "media_files"
+    end,
+  }
 }
--- plugins/community.lua
+++ plugins/community.lua
@@ -4,6 +4,18 @@ return {
   -- example of importing a plugin, comment out to use it or add your own
   -- available plugins can be found at https://github.com/AstroNvim/astrocommunity
 
-  -- { import = "astrocommunity.colorscheme.catppuccin" },
+  { import = "astrocommunity.colorscheme.rose-pine" },
+  { import = "astrocommunity.indent.mini-indentscope" },
+  { import = "astrocommunity.indent.indent-blankline-nvim" },
+  -- { import = "astrocommunity.pack.rust" },
+  {
+    "rose-pine",
+    opts = {
+      disable_background = true;
+      groups = {
+        -- background = '#f8f8f2aa'
+      }
+    },
+  },
   -- { import = "astrocommunity.completion.copilot-lua-cmp" },
 }
